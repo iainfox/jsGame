a7fb302b5eb8ddc29dbf58fcf81a8dcd1f452c36
therealpyppy <furghadmanar@gmail.com>
2025-07-22 23:51:13 -0500

    fix types

--- src/draw/shapes/circle.ts
+++ src/draw/shapes/circle.ts
@@ -1,31 +1,32 @@
+import { Color } from "../../core/color.ts";
 import { core } from "../../core/core.ts";
 
 export function circle(
-	ctx,
-	color,
-	center,
-	radius,
-	width = 0,
-	draw_top_right = false,
-	draw_top_left = false,
-	draw_bottom_left = false,
-	draw_bottom_right = false,
+	ctx: CanvasRenderingContext2D,
+	color: Color,
+	center: Array<number>,
+	radius: number,
+	width: number = 0,
+	draw_top_right: boolean = false,
+	draw_top_left: boolean = false,
+	draw_bottom_left: boolean = false,
+	draw_bottom_right: boolean = false,
 ) {
-	ctx.fillStyle = new core.Color(color).hex3;
-	ctx.strokeStyle = new core.Color(color).hex3;
+	ctx.fillStyle = color.hex3;
+	ctx.strokeStyle = color.hex3;
 	if (!Array.isArray(center) || center.length !== 2) {
 		throw new Error("center must be an array of two numbers [x, y]");
 	}
-	if (!typeof radius == "number" || !(radius % 1 == 0)) {
+	if (!(radius % 1 == 0)) {
 		throw new Error("radius must be an integer");
 	}
 
-	let x = center[0];
-	let y = center[1];
-	let top = y - radius;
-	let bottom = y + radius;
-	let left = x - radius;
-	let right = x + radius;
+	const x = center[0];
+	const y = center[1];
+	const top = y - radius;
+	const bottom = y + radius;
+	const left = x - radius;
+	const right = x + radius;
 
 	if (width < 0 || radius < 0) {
 		return new core.Rect([x, y], [0, 0]);
