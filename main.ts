4c8159b924d86f289cc87607f01042b0b4bc825c
therealpyppy <furghadmanar@gmail.com>
2025-07-22 22:25:18 -0500

    cahnge file extensions

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { jsgame } from "./src/jsgame.js"
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const canvas = document.getElementById("canvas")
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const surface = canvas.getContext('2d')
+
+let points = [
+	[25, 20],
+	[80, 26],
+	[180, 80],
+]
+
+jsgame.draw.lines(surface, "#FF0000", true, points, 1)
+
+jsgame.draw.ellipse(surface, "#FF0000", new jsgame.core.Rect([325, 80], [120, 60]), 0)
